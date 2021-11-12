0f68090cf10fbf496cb5efa069d0b545c7ee97a4
Beasley <63673533+beasleyDOTcom@users.noreply.github.com>
2021-11-11 20:56:41 -0800

    added Twilio and twilio.aspnet.core, added classes

---
+++ OpenMicNight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetDockerHerokuTwilio
+{
+    public class OpenMicNight
+    {
+        public string OpenMicName { get; set; }
+        public string RoomCode { get; set; }
+        public List<Performance> Performances { get; set; }
+        /*
+        public DateTime Date { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        */
+    }
+}
---
+++ Performer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnetDockerHerokuTwilio
+{
+    public class Performer
+    {
+        public string PhoneNumber { get; set; }
+        public string Instrument { get; set; }
+        public string PersonallyAnnouncedAs { get; set; }
+        public string Name { get; set; }
+    }
+}
